ea887be560d5b5be9720b39d6ea13ad1d0409784
yoshimalucky <y-tanaka@jsk.imi.i.u-tokyo.ac.jp>
2019-06-04 17:35:51 +0900

    Finished node.

--- t265_robot_tf_composer/src/t265_robot_tf_composer.cpp
+++ t265_robot_tf_composer/src/t265_robot_tf_composer.cpp
@@ -3,15 +3,28 @@
 void odomCallback(const nav_msgs::OdometryConstPtr& msg)
 {
   static tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped tfCameraToBody;
   geometry_msgs::TransformStamped tfCameraToHeadTilt;
   geometry_msgs::TransformStamped tfHeadTiltToHeadPan;
   geometry_msgs::TransformStamped tfHeadPanToBody;
   geometry_msgs::TransformStamped tfOdom;
 
   try {
+    tfCameraToBody = tfBuffer.lookupTransform("op3/body_link",
+                                              "camera_pose_frame",
+                                              ros::Time(0));
+  }
+  catch (tf2::TransformException &ex) {
+    ROS_WARN("%s",ex.what());
+    ros::Duration(1.0).sleep();
+    // continue;
+    return;
+  }
+
+  try {
     tfCameraToHeadTilt = tfBuffer.lookupTransform("camera_pose_frame",
-                                         "op3/head_tilt_link",
-                                         ros::Time(0));
+                                                  "op3/head_tilt_link",
+                                                  ros::Time(0));
   }
   catch (tf2::TransformException &ex) {
     ROS_WARN("%s",ex.what());
@@ -22,8 +35,8 @@ void odomCallback(const nav_msgs::OdometryConstPtr& msg)
 
   try {
     tfHeadTiltToHeadPan = tfBuffer.lookupTransform("op3/head_tilt_link",
-                                         "op3/head_pan_link",
-                                         ros::Time(0));
+                                                   "op3/head_pan_link",
+                                                   ros::Time(0));
   }
   catch (tf2::TransformException &ex) {
     ROS_WARN("%s",ex.what());
@@ -34,8 +47,8 @@ void odomCallback(const nav_msgs::OdometryConstPtr& msg)
 
   try {
     tfHeadPanToBody = tfBuffer.lookupTransform("op3/head_pan_link",
-                                         "op3/body_link",
-                                         ros::Time(0));
+                                               "op3/body_link",
+                                               ros::Time(0));
   }
   catch (tf2::TransformException &ex) {
     ROS_WARN("%s",ex.what());
@@ -44,14 +57,47 @@ void odomCallback(const nav_msgs::OdometryConstPtr& msg)
     return;
   }
 
+  if (count == 0) {
+    x_1 = tfCameraToBody.transform.translation.x;
+    // x_2 = tfHeadPanToBody.transform.translation.x;
+    y_1 = tfCameraToBody.transform.translation.y;
+    // y_2 = tfHeadTiltToHeadPan.transform.translation.y;
+    z_1 = tfCameraToBody.transform.translation.z;
+    // z_2 = tfHeadTiltToHeadPan.transform.translation.z;
+    ROS_INFO("init : %f", x_1);
+  } count++;
+
   tfOdom.header.stamp = ros::Time::now();
   tfOdom.header.frame_id = "odom";
   tfOdom.child_frame_id = "op3/body_link";
 
   // Transform
-  tfOdom.transform.translation.x = msg->pose.pose.position.x;
-  tfOdom.transform.translation.y = msg->pose.pose.position.y;
-  tfOdom.transform.translation.z = msg->pose.pose.position.z;
+  tfOdom.transform.translation.x = msg->pose.pose.position.x
+    // + tfCameraToBody.transform.translation.x
+    // + tfHeadPanToBody.transform.translation.x
+    - (tfCameraToBody.transform.translation.x - x_1)
+    // - (tfHeadTiltToHeadPan.transform.translation.x - x_2)
+    // - x_1 - x_2;
+    ; // + or - tf caused by rotation
+
+  ROS_INFO("odom x : %f", msg->pose.pose.position.x);
+  ROS_INFO("tf from body to cam : %f", tfCameraToBody.transform.translation.x);
+  ROS_INFO("init : %f", x_1);
+
+  tfOdom.transform.translation.y = msg->pose.pose.position.y
+    // - tfCameraToBody.transform.translation.y
+    // + tfHeadPanToBody.transform.translation.y
+    - (tfCameraToBody.transform.translation.y - y_1)
+    // - (tfHeadTiltToHeadPan.transform.translation.y - y_2)
+    // - y_1 - y_2;
+    ; // same as x
+  tfOdom.transform.translation.z = msg->pose.pose.position.z
+    // - tfCameraToBody.transform.translation.z
+    // + tfHeadPanToBody.transform.translation.z
+    - (tfCameraToBody.transform.translation.z - z_1)
+    // - (tfHeadTiltToHeadPan.transform.translation.z - z_2)
+    // - z_1 - z_2;
+    ; // same as x, y
 
   // Rotation
   // tfCameraToHeadTilt tfHeadTiltToHeadPan tfHeadPanToBody tfOdom
@@ -61,7 +107,7 @@ void odomCallback(const nav_msgs::OdometryConstPtr& msg)
   tf2::convert(tfCameraToHeadTilt.transform.rotation, camHeadTiltQ);
   tf2::convert(msg->pose.pose.orientation, msgQ);
 
-  odomQ = headPanBodyQ * headTiltHeadPanQ  * camHeadTiltQ * msgQ;
+  odomQ = camHeadTiltQ * headPanBodyQ * headTiltHeadPanQ * msgQ;
   odomQ.normalize();
 
   tf2::convert(odomQ, tfOdom.transform.rotation);
@@ -72,6 +118,8 @@ void odomCallback(const nav_msgs::OdometryConstPtr& msg)
 
 int main(int argc, char **argv)
 {
+  x_1 = 0; x_2 = 0; y_1 = 0; y_2 = 0; z_1 =0; z_2 = 0;
+
   ros::init(argc, argv, "t265_robot_tf_composer");
 
   ros::NodeHandle nh;
